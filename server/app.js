f79ab2884009333b7d7adda418caa229740b6d5a
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-03-06 21:43:20 +0300

    entry file for server

---
+++ server/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
+
+// ./package.json
